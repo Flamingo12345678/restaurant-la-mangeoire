0d6ffbf5f1ed2611655c40ef94758bb29640ae3c
Flamingo <ernestyombi20@gmail.com>
2025-04-29 02:33:41 +0200

    mise a jour de la configuration railway

--- db_connexion.php
+++ db_connexion.php
@@ -4,8 +4,9 @@ $host = getenv('MYSQLHOST') ?: 'localhost';
 $db   = getenv('MYSQLDATABASE') ?: 'restaurant_la_mangeoire';
 $user = getenv('MYSQLUSER') ?: 'root';
 $pass = getenv('MYSQLPASSWORD') ?: '';
+$port = getenv('MYSQLPORT') ?: '3306';
 $charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
